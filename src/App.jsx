07d20156a05f265531de6cc670bc4e29541e1668
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-02-19 21:49:58 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Contacts } from "./features/contacts/Contacts";
+
+export function App() {
+  return ( 
+    <div>
+      <h1>Phonebook</h1>
+      <Contacts/>
+    </div>
+   );
+}
