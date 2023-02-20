f50a7f455e53e172a670bd26fd287ac7f2a19aab
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-02-20 23:02:52 +0100

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import contactsReducer from "../features/contacts/ContactsSlice";
-import filterReducer from "../features/filters/FiltersSlice"
+import filtersReducer from "../features/filters/FiltersSlice"
 
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
-    filters: filterReducer,
+    filters: filtersReducer,
   },
 });
