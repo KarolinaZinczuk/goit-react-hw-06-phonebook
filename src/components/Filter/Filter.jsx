3288acc95764dac999b59cd458195559e04f81d2
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-02-21 13:00:10 +0100

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,18 +1,22 @@
-import PropTypes from "prop-types";
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setFilter, selectFilters } from 'redux/FiltersSlice';
+
 import styles from "./Filter.module.css";
 
-const Filter = ({value, onChange}) => {
+export const Filter = () => {
+    const filters = useSelector(selectFilters);
+    const dispatch = useDispatch();
+
+    const onFilterChange = e => {
+    const input = e.target.value;
+    dispatch(setFilter(input));
+    };
+
     return (
         <div className={styles.section}>
             <h2>Find contacts by name</h2>
-            <input className={styles.input} onChange={onChange} value={value}></input>
+            <input className={styles.input} name="input" onChange={onFilterChange} value={filters}></input>
         </div>
     )
 };
-
-Filter.propTypes = {
-    value: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-}
-
-export default Filter ;
