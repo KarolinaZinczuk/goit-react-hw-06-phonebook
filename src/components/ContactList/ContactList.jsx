5e7fd3819ee833e53c3e859dcc7713f50758ac7a
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-02-22 14:35:24 +0100

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,37 +1,30 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { deleteContact, selectContacts } from 'redux/ContactsSlice';
+import { selectContacts, selectFilteredContacts } from 'redux/selectors.js';
+import { deleteContact } from 'redux/ContactsSlice';
+
 import PropTypes from "prop-types";
 import styles from "./ContactList.module.css";
-import { selectFilters } from 'redux/FiltersSlice';
 
 export const ContactList = () => {
     const contacts = useSelector(selectContacts);
-    const filters = useSelector(selectFilters);
+    const filteredContacts = useSelector(selectFilteredContacts);
     const dispatch = useDispatch();
 
-    const filteredContacts = contacts.filter(contact => {
-    return contact.name.toLowerCase().includes(filters.toLowerCase());
-  });
-
     return (
-        <>
-            {filteredContacts.length > 0 ? (
-                <ul className={styles.list}>
-                    {contacts.length ? contacts.map((contact) => (
-                        <li className={styles.item} key={contact.id}>
-                            {contact.name} {contact.number}
-                            <button
-                                className={styles.button}
-                                onClick={() => dispatch(deleteContact(contact.id))}>
-                                Delete
-                            </button>
-                        </li>
-                    ))
-                        : 'No contacts'}
-                </ul>
-            ) : "You don't have this contact"}
-        </>
+        <ul className={styles.list}>
+            {contacts.length ? filteredContacts.map((contact) => (
+                <li className={styles.item} key={contact.id}>
+                    {contact.name} {contact.number}
+                    <button
+                        className={styles.button}
+                        onClick={() => dispatch(deleteContact(contact.id))}>
+                        Delete
+                    </button>
+                </li>
+            ))
+                : 'No contacts'}
+        </ul>
     );
 };
 
