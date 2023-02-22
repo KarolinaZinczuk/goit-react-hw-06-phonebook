f136327fef6f85b7d7eab93bdb4d1183eb030385
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-02-22 07:13:46 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useSelector} from 'react';
+
+import { selectFilters } from 'redux/FiltersSlice';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
+import { selectContacts } from 'redux/ContactsSlice';
 
-export const App = () => {  
+export const App = () => {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectFilters);
+
   return (
     <div>
       <h1>Phonebook</h1>
