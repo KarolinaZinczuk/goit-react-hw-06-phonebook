67ba68d5962e7a5131a11182466046ae292579f4
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-02-22 07:13:14 +0100

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { setFilter, selectFilters } from 'redux/FiltersSlice';
+import { useDispatch } from 'react-redux';
+import { setFilter } from 'redux/FiltersSlice';
 
 import styles from "./Filter.module.css";
 
 export const Filter = () => {
-    const filters = useSelector(selectFilters);
     const dispatch = useDispatch();
 
     const onFilterChange = e => {
-    const input = e.target.value;
-    dispatch(setFilter(input));
+        const input = e.target.value;
+        dispatch(setFilter(input));
     };
 
     return (
         <div className={styles.section}>
             <h2>Find contacts by name</h2>
-            <input className={styles.input} name="input" onChange={onFilterChange} value={filters}></input>
+            <input
+                className={styles.input}
+                type="text"
+                name="filter"
+                onChange={onFilterChange}>             
+            </input>
         </div>
     )
 };
