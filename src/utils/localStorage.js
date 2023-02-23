a2033482b3f6329ff30bb149e49163c1fe0ac79f
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-02-23 19:05:49 +0100

    Create localStorage.js

---
+++ src/utils/localStorage.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+
+const useLocalStorage = (key, value) => {
+  const [state, setState] = useState(() => {
+    return JSON.parse(localStorage.getItem(key)) ?? value;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+  return [state, setState];
+};
+
+export default useLocalStorage;
